1bd430c2dbca65bac93ba860e11c22dcd3f69116
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-22 15:52:02 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,8 @@
 export default function getResponseFromAPI() {
-    let myPromise = new Promise(
-	    myResolve();
-	);
-	return myPromise;
+    const myPromise = new Promise(
+    (resolve) => {
+      resolve();
+    },
+  );
+  return myPromise;
 }
