21dee60b72d8b8c243d080d00a3b649500fca7cd
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-27 14:00:56 +0000

    5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,17 @@
+export default function createInt8TypedArray(length, position, value) {
+  // Create a new ArrayBuffer with the given length
+  const buffer = new ArrayBuffer(length);
+  
+  // Create a new Int8Array view on the buffer
+  const int8Array = new Int8Array(buffer);
+  
+  // Check if the position is within range
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  
+  // Set the value at the given position
+  int8Array[position] = value;
+  
+  return buffer;
+}
