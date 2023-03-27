e421a9f9e38627bff4f01fba225a892b1b3a1c8b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-27 13:36:36 +0000

    1-get_list_student_ids.js

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+// If the input is not an array
+export default function getListStudentIds(studentArray) {
+  // If the input is not an empty array
+  if (!Array.isArray(studentArray)) {
+    return [];
+  }
+  
+  // Use map to extract the IDs object in the array
+  const idArray = studentArray.map(student => student.id);
+  
+  return idArray;
+}
