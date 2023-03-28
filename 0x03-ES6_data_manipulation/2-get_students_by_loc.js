78cf675bc9578c568373b5a30f8f01811c2f2902
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-28 09:20:51 +0000

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(studentArray, city) {
   // Use filter to find all students located in the given city
-  const matchingStudents = studentArray.filter(student => student.location === city);
-  
+  const matchingStudents = studentArray.filter((student) => student.location === city);
+
   return matchingStudents;
 }
