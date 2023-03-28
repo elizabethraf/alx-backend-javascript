d5f8096df4c932f5bb8b075d0ef9623101dace09
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-28 09:23:38 +0000

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(studentArray) {
   // Use reduce to add up all the student IDs in the array
   const idSum = studentArray.reduce((acc, student) => acc + student.id, 0);
-  
+
   return idSum;
 }
